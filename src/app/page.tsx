8488d36d4e4ce004abb4501491ee701c1308d743
maryna <maryna@cloudomation.com>
2025-10-31 09:46:21 +0100

    create a project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import { Button } from "@nextui-org/react";
-
 export default function Home() {
-  return (
-    <div>
-      <Button>Click me!</Button>
-    </div>
-  );
+  return <div>Home page</div>;
 }
