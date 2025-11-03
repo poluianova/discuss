19afbd93c48a218b774301aa35196cb46bcf8809
maryna <maryna@cloudomation.com>
2025-11-03 13:04:07 +0100

    additional comment

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,3 +1,23 @@
-export default function TopicShowPage() {
-  return <div>Topic show</div>;
+import PostCreateForm from "@/components/posts/post-create-form";
+import { fetchPostsByTopicSlug } from "@/db/queries/posts";
+import PostList from "@/components/posts/post-list";
+interface TopicShowPageProps {
+  params: Promise<{ slug: string }>;
+}
+
+export default async function TopicShowPage({ params }: TopicShowPageProps) {
+  const { slug } = await params;
+
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+        <PostList fetchData={() => fetchPostsByTopicSlug(slug)} />
+      </div>
+
+      <div>
+        <PostCreateForm slug={slug} />
+      </div>
+    </div>
+  );
 }
